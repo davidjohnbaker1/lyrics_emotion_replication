d8a2949522d0e8c043d7ea1633a5bc1a9e42197a
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2020-12-10 11:09:26 +0000

    pre submission 1

---
+++ R/update_data.R
@@ -0,0 +1,41 @@
+#--------------------------------------------------
+# UPDATE DATA SCRIPT
+#--------------------------------------------------
+# Run Me to Updata Datafiles
+#--------------------------------------------------
+library(tidyverse)
+#--------------------------------------------------
+source("R/data_cleaner.R")
+setwd("data/raw/experimental/")
+score_emotional_data()
+setwd("processed/")
+create_emo_dataset()
+junk <- dir(pattern="_data") 
+file.remove(junk)
+setwd("../../../..")
+#--------------------------------------------------
+source("R/score_gmsi_extra.R")
+setwd("data/raw/gmsi/")
+score.gmsi.extra()
+create.gmsi.dataset()
+junk <- dir(pattern="_data") 
+file.remove(junk)
+setwd("../../../")
+#--------------------------------------------------
+# Make Master
+
+experimental_data <- read_csv("data/cooked/experimental_data.csv")
+gmsi_data <- read_csv("data/Current_GMSI_Data.csv")
+
+gmsi_data %>%
+  rename(subject = subjectNo) -> gmsi_data
+
+#View(gmsi_data)
+
+experimental_data %>%
+  select(-X1) %>%
+  left_join(gmsi_data) -> joined_data
+
+#View(joined_data)
+
+write_csv(joined_data, "Complete_Data.csv")
